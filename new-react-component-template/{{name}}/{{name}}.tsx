79d08300bd6926c482116e01970c160b00ba9a53
uriSecfi <uri@secfi.com>
2019-02-07 15:25:00 +0100

    Update {{name}}.tsx
    TEST

--- new-react-component-template/{{name}}/{{name}}.tsx
+++ new-react-component-template/{{name}}/{{name}}.tsx
@@ -1,9 +1,6 @@
 import "./NewComponent.css";
 import * as React from "react";
-import { observer, inject } from "mobx-react";
 
-@inject("")
-@observer
 export default class {{name}} extends React.Component<any, any> {
   public render() {
     return <div className="{{name}}">{{name}}</div>;
